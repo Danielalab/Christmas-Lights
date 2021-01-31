b12d6948c0f7b82492c4babb91a1ef7dc339cb3c
Danielalab <olabolacrayola@hotmail.com>
2021-01-31 16:17:48 -0500

    feat(button): add event to button On

--- index.js
+++ index.js
@@ -5,5 +5,13 @@ const globes = document.querySelectorAll('.globe');
 buttonOff.addEventListener('click', () => {
   [...globes].forEach(globeElement => {
     globeElement.classList.add('paused-animation');
+    globeElement.classList.remove('running-animation');
+  });
+});
+
+buttonOn.addEventListener('click', () => {
+  [...globes].forEach(globeElement => {
+    globeElement.classList.remove('paused-animation');
+    globeElement.classList.add('running-animation');
   });
 });
