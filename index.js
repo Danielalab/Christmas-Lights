a65fe673f335560cbb7a0a5e2d7214ccfa49c9f9
Danielalab <olabolacrayola@hotmail.com>
2021-02-05 15:44:39 -0500

    feat(select-intensity): add event to change shadow intensity

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const buttonOn = document.querySelector('button[data-action="on"]');
 const buttonOff = document.querySelector('button[data-action="off"]');
-const selectToChangeSpeed = document.querySelector('select');
+const selectToChangeSpeed = document.querySelector('select[name=change-speed]');
+const selectToChangeIntensity = document.querySelector('select[name=change-intensity]');
 const globes = document.querySelectorAll('.globe');
 
 buttonOff.addEventListener('click', () => {
@@ -21,5 +22,12 @@ selectToChangeSpeed.addEventListener('change', () => {
   const speedValue = (selectToChangeSpeed.value);
   [...globes].forEach(globe => {
     globe.style.animationDuration = speedValue + 's';
+  });
+});
+
+selectToChangeIntensity.addEventListener('change', () => {
+  const intensityValue = (selectToChangeIntensity.value);
+  [...globes].forEach(globe => {
+    globe.style.boxShadow = `2px 2px 15px ${intensityValue}px`;
   });
 });
