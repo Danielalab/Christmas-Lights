1e4482103f3d1355b179f28da2cc12edd7d6226c
Danielalab <olabolacrayola@hotmail.com>
2021-02-06 14:44:12 -0500

    feat(number-globes): add event to create globes

--- index.js
+++ index.js
@@ -2,9 +2,31 @@ const buttonOn = document.querySelector('button[data-action="on"]');
 const buttonOff = document.querySelector('button[data-action="off"]');
 const selectToChangeSpeed = document.querySelector('select[name=change-speed]');
 const selectToChangeIntensity = document.querySelector('select[name=change-intensity]');
-const globes = document.querySelectorAll('.globe');
+const inputNumberOfGlobes = document.querySelector('input[name="number-of-globes"]');
+const btnToCreateGlobes = document.querySelector('button[data-action="create-globes"]');
+
+const createGlobes = (numberOfGlobes) => {
+  let result;
+  if (numberOfGlobes === 0) {
+    result = [];
+  } else if (numberOfGlobes >= 1) {
+    const div = document.createElement('div');
+    div.classList.add('globe');
+    result = [div].concat(createGlobes(numberOfGlobes - 1));
+  }
+  return result;
+}
+
+const showGlobes = (numberOfGlobes) => {
+  const containerGlobesElement = document.querySelector('.globes-container');
+  containerGlobesElement.innerHTML = '';
+  containerGlobesElement.append(...createGlobes(numberOfGlobes));
+}
+
+/* Events */
 
 buttonOff.addEventListener('click', () => {
+  const globes = document.querySelectorAll('.globe');
   [...globes].forEach(globeElement => {
     globeElement.classList.add('paused-animation');
     globeElement.classList.remove('running-animation');
@@ -12,6 +34,7 @@ buttonOff.addEventListener('click', () => {
 });
 
 buttonOn.addEventListener('click', () => {
+  const globes = document.querySelectorAll('.globe');
   [...globes].forEach(globeElement => {
     globeElement.classList.remove('paused-animation');
     globeElement.classList.add('running-animation');
@@ -19,6 +42,7 @@ buttonOn.addEventListener('click', () => {
 });
 
 selectToChangeSpeed.addEventListener('change', () => {
+  const globes = document.querySelectorAll('.globe');
   const speedValue = (selectToChangeSpeed.value);
   [...globes].forEach(globe => {
     globe.style.animationDuration = speedValue + 's';
@@ -26,8 +50,14 @@ selectToChangeSpeed.addEventListener('change', () => {
 });
 
 selectToChangeIntensity.addEventListener('change', () => {
+  const globes = document.querySelectorAll('.globe');
   const intensityValue = (selectToChangeIntensity.value);
   [...globes].forEach(globe => {
     globe.style.boxShadow = `2px 2px 15px ${intensityValue}px`;
   });
+});
+
+btnToCreateGlobes.addEventListener('click', () => {
+  const numberOfGlobesValue = parseInt(inputNumberOfGlobes.value);
+  showGlobes(numberOfGlobesValue);
 });
